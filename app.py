1869863a2a6806b5ffefb7262bd092cb7e0eb180
Sajjan Poudel <sajjanpoudel05@gmail.com>
2023-12-18 11:24:15 +0000

    first version donein just one day lol

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
-import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.metrics import mean_squared_error
-import pandas as pd
 from joblib import dump
 
 app = Flask(__name__)
@@ -13,8 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.secret_key = 'secret_key'
 
-# --user authentication--
-
+# User authentication
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -29,40 +26,31 @@ class User(db.Model):
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
 
-with app.app_context():
-    db.create_all()
+def create_tables():
+    with app.app_context():
+        db.create_all()
 
-# Prediction
-def preprocess_data(car_data):
-    # Load your dataset (replace 'your_dataset.csv' with the actual file name)
-    df = pd.read_csv('resources/carsData.csv')
-    # One-hot encode categorical variables
-    df_encoded = pd.get_dummies(df, columns=['companyName', 'carModel', 'engineType', 'showroom'])
 
-    # Assuming 'price' is still the target variable
-    X_encoded = df_encoded.drop('price', axis=1)
-    y_encoded = df_encoded['price']
 
-    # Split the data into training and testing sets
-    X_train_encoded, X_test_encoded, y_train_encoded, y_test_encoded = train_test_split(
-        X_encoded, y_encoded, test_size=0.2, random_state=42)
+# Load and preprocess data
+df = pd.read_csv('resources/carsData.csv')
+df_encoded = pd.get_dummies(df, columns=['companyName', 'carModel', 'engineType', 'showroom'])
+X_encoded = df_encoded.drop('price', axis=1)
+y_encoded = df_encoded['price']
+X_train_encoded, X_test_encoded, y_train_encoded, y_test_encoded = train_test_split(
+    X_encoded, y_encoded, test_size=0.2, random_state=42)
 
-    # Train Linear Regression model
-    linear_model = LinearRegression()
-    linear_model.fit(X_train_encoded, y_train_encoded)
+# Train Linear Regression model
+linear_model = LinearRegression()
+linear_model.fit(X_train_encoded, y_train_encoded)
 
-    # Train Lasso Regression model
-    lasso_model = Lasso(alpha=0.1)  # Adjust alpha as needed
-    lasso_model.fit(X_train_encoded, y_train_encoded)
+# Train Lasso Regression model
+lasso_model = Lasso(alpha=0.1)
+lasso_model.fit(X_train_encoded, y_train_encoded)
 
-    # Save the trained models using joblib
-    dump(linear_model, 'linear_model.joblib')
-    dump(lasso_model, 'lasso_model.joblib')
-
-    # Return the models and test data for later use
-    return linear_model, lasso_model, X_test_encoded, y_test_encoded
-
-linear_model, lasso_model, X_test_encoded, y_test_encoded = preprocess_data(None)
+# Save the trained models using joblib
+dump(linear_model, 'linear_model.joblib')
+dump(lasso_model, 'lasso_model.joblib')
 
 @app.route('/')
 def index():
@@ -71,7 +59,6 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # handle request
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
@@ -115,7 +102,7 @@ def logout():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Extract data from the form
+        # Extract form data
         companyName = request.form['companyName']
         carModel = request.form['carModel']
         yearMade = int(request.form['yearMade'])
@@ -123,30 +110,32 @@ def predict():
         engineType = request.form['engineType']
         showroom = request.form['showroom']
 
-        # Create a dictionary of input data
-        car_data = {
+        # Create a DataFrame from the input data
+        data = pd.DataFrame([{
             'companyName': companyName,
             'carModel': carModel,
             'yearMade': yearMade,
             'kmsRan': kmsRan,
             'engineType': engineType,
             'showroom': showroom
-        }
+        }])
 
-        # Unpack the returned values from preprocess_data
-        linear_model, lasso_model, X_test_encoded, y_test_encoded = preprocess_data(car_data)
+        # One-hot encode categorical variables
+        data_encoded = pd.get_dummies(data, columns=['companyName', 'carModel', 'engineType', 'showroom'])
 
-        # Predict using Linear Regression model
-        linear_prediction = linear_model.predict(X_test_encoded)
+        # Ensure columns consistency with the training data
+        for col in X_train_encoded.columns:
+            if col not in data_encoded.columns:
+                data_encoded[col] = 0
 
-        # Predict using Lasso Regression model
-        lasso_prediction = lasso_model.predict(X_test_encoded)
+        # Reorder columns to match the order during training
+        data_encoded = data_encoded[X_train_encoded.columns]
 
-        # Calculate the mean squared error for each model (optional)
-        linear_rmse = mean_squared_error(y_test_encoded, linear_prediction, squared=False)
-        lasso_rmse = mean_squared_error(y_test_encoded, lasso_prediction, squared=False)
+        # Predict using models
+        linear_prediction = linear_model.predict(data_encoded)
+        lasso_prediction = lasso_model.predict(data_encoded)
 
-         # Format predictions as Nepali Rupees
+        # Format predictions as Nepali Rupees
         formatted_linear_prediction = "Rs: {:.0f}".format(linear_prediction[0] * 100000)
         formatted_lasso_prediction = "Rs: {:.0f}".format(lasso_prediction[0] * 100000)
 
@@ -155,8 +144,8 @@ def predict():
             linear_prediction=formatted_linear_prediction,
             lasso_prediction=formatted_lasso_prediction
         )
-    return render_template('dashboard.html')
 
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
